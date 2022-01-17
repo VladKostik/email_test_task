919750ef9868609b3fae91cf5af68f10be3a9048
VladKostik <udsa.staff@gmail.com>
2022-01-17 08:50:24 +0200

    *Done - updated data

--- pages/core/get_sign_in_data.py
+++
@@ -1,8 +0,0 @@
-with open('../pages/core/data', 'r') as file:
-    lines = file.readlines()
-    email = lines[0]
-    password = lines[1]
-    for line in lines:
-        print(line)
-
-file.close()
